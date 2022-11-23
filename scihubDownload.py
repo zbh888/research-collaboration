9320e8c5bd01214ac73288a51203873ea4bc22bb
Bohan Zhang <b327zhan@uwaterloo.ca>
2022-11-23 18:56:44 -0500

    Add files via upload

---
+++ scihubDownload.py
@@ -0,0 +1,8 @@
+from scidownl import scihub_download
+
+def downloadPDF(DOIs, path):
+    source = []
+    for doi in DOIs:
+        source.append((doi, 'doi', path))
+    for paper, paper_type, out in source:
+        scihub_download(paper, paper_type=paper_type, out=out)
