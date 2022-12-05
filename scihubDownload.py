734c81602f4b312e700dadde30b986a7acb6f292
Bohan Zhang <b327zhan@uwaterloo.ca>
2022-12-05 13:58:31 -0500

    Delete scihubDownload.py

--- scihubDownload.py
+++
@@ -1,8 +0,0 @@
-from scidownl import scihub_download
-
-def downloadPDF_sci(DOIs, path):
-    source = []
-    for doi in DOIs:
-        source.append((doi, 'doi', path))
-    for paper, paper_type, out in source:
-        scihub_download(paper, paper_type=paper_type, out=out)
