681e296e292a1cc5e88ceb19191ad104230be4c5
Bohan Zhang <b327zhan@uwaterloo.ca>
2022-12-07 11:23:01 -0500

    Update extractFirstPage.py

--- extractFirstPage.py
+++ extractFirstPage.py
@@ -10,12 +10,16 @@ def extractFirstPage(path):
         if filename.endswith(".pdf"):
             pdf_files.append(filename)
     pdf_files.sort(key=str.lower)
-    
-    for filename in pdf_files:
-        reader = PdfFileReader(path + filename)
-        writer = PdfFileWriter()
-        page = reader.getPage(0)
-        writer.addPage(page)
 
-        with open(p + filename, "wb") as fp:
-            writer.write(fp)
+
+    for filename in pdf_files:
+        try:
+            reader = PdfFileReader(path + filename, strict = False)
+            writer = PdfFileWriter()
+            page = reader.getPage(0)
+            writer.addPage(page)
+
+            with open(p + filename, "wb") as fp:
+                writer.write(fp)
+        except:
+            print("ERROR:", path + filename)
