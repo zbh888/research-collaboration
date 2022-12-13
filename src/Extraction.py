5834e4bee624b61d867ce927a95e596d50803b44
Bohan Zhang <b327zhan@uwaterloo.ca>
2022-12-13 13:29:42 -0500

    Update Extraction.py

--- src/Extraction.py
+++ src/Extraction.py
@@ -2,7 +2,6 @@ import xml.etree.ElementTree as ET
 
 # example: affiliationExtraction("test.xml")
 
-import xml.etree.ElementTree as ET
 def affiliationExtraction(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
@@ -34,6 +33,32 @@ def titleExtraction(xml_file):
     for title in root.iter("{http://www.tei-c.org/ns/1.0}title"):
         res.append(title.text)
     return res
+
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import os
+
+# example extractFirstPage('./papers/')
+def extractFirstPage(path):
+    p = path+'FirstPage/'
+    os.mkdir(p)
+    pdf_files = []
+    for filename in os.listdir(path):
+        if filename.endswith(".pdf"):
+            pdf_files.append(filename)
+    pdf_files.sort(key=str.lower)
+
+
+    for filename in pdf_files:
+        try:
+            reader = PdfFileReader(path + filename, strict = False)
+            writer = PdfFileWriter()
+            page = reader.getPage(0)
+            writer.addPage(page)
+
+            with open(p + filename, "wb") as fp:
+                writer.write(fp)
+        except:
+            print("ERROR:", path + filename)
 
 
 
