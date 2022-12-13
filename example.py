253a1d69d5b2f89defd62e6b767b2609360a4c33
Bohan Zhang <b327zhan@uwaterloo.ca>
2022-12-13 11:57:59 -0500

    Delete example.py

--- example.py
+++
@@ -1,7 +0,0 @@
-from Query import query, collectDoi
-from scihubDownload import downloadPDF_sci
-from acmDownload import downloadPDF_ACM
-res = query(2021, 2021 ,"IEEE Symposium on Security and Privacy")
-doi = collectDoi(res)
-downloadPDF_sci(doi, './papers/')
-downloadPDF_ACM(doi, './papers/')
