1e06588ed2c5245ffe54e7fed31c85eb51f63a59
Bohan Zhang <b327zhan@uwaterloo.ca>
2022-12-18 20:03:12 -0500

    Delete parsePDF.py

--- parsePDF.py
+++
@@ -1,5 +0,0 @@
-from grobid_client.grobid_client import GrobidClient
-
-if __name__ == "__main__":
-    client = GrobidClient(config_path="./grobid_client_python/config.json")
-    client.process("processHeaderDocument", "./pdfs", output="./pdfs/xml", tei_coordinates=True, force=True)
